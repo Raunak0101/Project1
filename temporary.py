d57f2e99d50888b49014b433e521737bbb6f0f87
Raunak0101 <raunakchatterjee.cii@gmail.com>
2025-05-03 11:09:24 +0530

    project structure

--- temporary.py
+++ temporary.py
@@ -1,4 +1,4 @@
-import os 
+import os ##finds the generic folder path
 from pathlib import Path 
 import logging 
 
@@ -7,7 +7,6 @@ logging.basicConfig(level=logging.INFO)
 project_name = "mlproject"
 
 list_of_file = [
-    "github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/componemts/__init__.py",
     f"src/{project_name}/componemts/data_ingestion.py",
@@ -21,6 +20,7 @@ list_of_file = [
     f"src/{project_name}/logger.py",
     f"src/{project_name}/utlis.py",
     "app.py",
+    "main.py",
     "Dockerfile",
     "requirements.txt",
     "setup.py"
@@ -30,3 +30,19 @@ list_of_file = [
 
 
 ]
+
+for file_path in list_of_file:
+    filepath = Path(file_path)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok= True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath,'w') as f:
+            pass 
+            logging.info(f"Creating empty file:{filepath}")
+
+    else:
+        logging.info(f"{filename} is already exists")
